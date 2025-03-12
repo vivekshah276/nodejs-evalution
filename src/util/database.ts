62a0296fd1c24ead4ab432d3eb56d3ffa86f7590
vivek shah <vivek.shah@seaflux.tech>
2025-03-12 17:06:52 +0530

    feat: forgot and reset password

--- src/util/database.ts
+++ src/util/database.ts
@@ -1,8 +1,6 @@
 import { Sequelize } from "sequelize";
 import config from "../config.js";
-console.log(config.db_name)
-console.log(config.db_password)
-console.log(config.db_username)
+
 
 const sequelize = new Sequelize(config.db_name, config.db_username , config.db_password, {
   dialect: "mysql",
